4ef67f9f5a29adab3d01348ea21b0ad7f4894b3d
xuxunsheng <xunsheng.xu@everxyz.com>
2023-08-28 22:11:56 +0800

    optimize

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 可以通过 format-text.vsix 本地安装，暂未发布到应用市场。
 
 ## 插件使用
-使用快捷键 cmd+shift+p 打开命令面板，输入 format-text 选择格式化中文。
+使用快捷键 cmd+shift+p 打开命令面板，输入 format-text 选择格式化文档。
 
 ## 参考资料
 VScode 官网基于 yo 框架开发插件 https://code.visualstudio.com/api/get-started/your-first-extension
--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-console.log("hello world");
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
